1cc09a5be4fd30a8a8f53b785df67ac9838e4fa7
Reza <srh.umir@gmail.com>
2016-02-26 10:38:25 +0100

    Read spot lst modified

--- ReadSPOTNDVI.R
+++ ReadSPOTNDVI.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(raster)
 ##read spotndvi and surrondings
 ##make a data.table which keep the spotndvi of each pixels and all of
 ##its surrondings. NDVI5 is the pixel. 
@@ -6,52 +7,49 @@ library(data.table)
 ##kepping surrondings is to use in NN
 ## to save memory, just unique values are saved. Converting it back to raster
 ##is another chalenge.
+SPOTNDVI15 <- raster(matrix(c(1:400, -20:-1), 21,20))
+
 
 SPOTNDVI15dt <- data.table( NDVI1 = numeric(), NDVI2 = numeric(), NDVI3 = numeric(), NDVI4 = numeric(), NDVI5 = numeric(), NDVI6 = numeric(), NDVI7 = numeric(), NDVI8 = numeric(), NDVI9 = numeric())
 r1 <- numeric(nrow(SPOTNDVI15)+2) *NA
 r2 <- r1
 r3 <- round(getValues(SPOTNDVI15, row = 1), digits = 2)
-for (j in 2:nrow(SPOTNDVI15)+1){
+r3 <- c(r3[length(r3)],r3,r3[1])
+for (j in 2:(nrow(SPOTNDVI15)+1)){
         if(j <= nrow(SPOTNDVI15)){
                 r1 <- r2
                 r2 <- r3
-                r3 <- round(getValues(SPOTNDVI15, row = 2), digits = 2)
-                r3 <- c(NA,r3,NA)
-        }
-        else{
+                r3 <- round(getValues(SPOTNDVI15, row = j), digits = 2)
+                r3 <- c(r3[length(r3)],r3,r3[1])
+        }else{
                 r1 <- r2
                 r2 <- r3
                 r3 <- numeric(nrow(SPOTNDVI15)+2) * NA
         }
         l = list()
-        for (i in 2:(nrow(SPOTNDVI15)-1)){
-                l[i] <- data.table(r1[i-1], )
+        for (i in 2:(length(r1)-1)){
+                if (r2[i] < .15 | is.na(r2[i])) next()
+                l[[i-1]] <- data.table(r1[i-1], r1[i], r1[i+1],
+                                       r2[i-1], r2[i], r2[i+1],
+                                       r3[i-1], r3[i], r3[i+1])
+        }
+        SPOTNDVI15dt <- unique(rbindlist(c(list(SPOTNDVI15dt),l)))
+        #SPOTNDVI15dt <- (rbindlist(c(list(SPOTNDVI15dt),l)))
+        percent <- floor((j*100)/nrow(SPOTNDVI15))
+        percentold <- floor(((j-1)*100)/nrow(SPOTNDVI15))
+        if (percent > percentold){
+                print(paste(as.character(percentold), "%"))
         }
 }
+SPOTNDVI15dt
 
+SPOTNDVI15dt <- cbind(SPOTNDVI15dt,.1+ 1.2*SPOTNDVI15dt)
+names(SPOTNDVI15dt)[10:18] <- paste(names(SPOTNDVI15dt)[10:18], "MODISi", sep = "")
+SPOTNDVI15dt[, LST_ := 0.1+0.2*SPOTNDVI15dt$NDVI5MODISi+0.3*SPOTNDVI15dt$NDVI5MODISi^2]
+SPOTNDVI15dt[,residuals := compute(nn,SPOTNDVI15dt[,10:18])]
+SPOTNDVI15[, LST := SPOTNDVI15dt$LST_+SPOTNDVI15dt$residuals]
 
-
-##convert SPOT NDVI to MODIs environment
-SPOTNDVI15dt <- data.table(SPOTNDVI = unique(round(getValues(SPOTNDVI15,  row = 1), digits = 2)))
-for (i in 2:16648){
-  SPOTNDVI15dt <- data.table(SPOTNDVI = unique(c(SPOTNDVI15dt$SPOTNDVI, 
-                                                 round(getValues(SPOTNDVI15,  row = i), digits = 2))))
-  print(paste(as.character((i*100/16648)),"%"))
-}
-
-##this one shoud be faster becaus is multicore
-#library(multicore)
-#l <- mclapply(1:nrows(SPOTNDVI15), function(i) 
-#              { data.table(SPOTNDVI = unique(round(getValues(SPOTNDVI15,  row = i), digits = 2)))
-#              })
-
-
-
-#SPOTNDVI15dt <- data.table(SPOTNDVI = getValues(SPOTNDVI15, ))
-#SPOTNDVI15MODIS <- lm$coefficients[1] + lm$coefficients[2] * SPOTNDVI15
-
-##This system works for NDVI >= 0.15. So we remove smaller NDVIs and work with data table for speed.
-
+getValuesFocal(SPOTNDVI15, ngb = 3)
 
 
 ##convert to MODIS environment
@@ -70,13 +68,3 @@ SPOTLST_ <- pol$coefficients[1] + pol$coefficients[2] * SPOTNDVI15MODIS +
 writeRaster(SPOTLST_, filename = "SPOTLSTbeforeNN.tif")
 
 
-##Prepare data to give to NN prepared in NeuralNetwork.R for computing residuals.
-reslist <- list()
-for (i in 1:9){
-  matrix <- matrix(0,3,3)
-  matrix[i] <- 1
-  #print(matrix)
-  reslist[i] <- focal(SPOTLST_,matrix, mean, na.rm = FALSE)
-}
-NDVIforNN <- as.data.frame(brick(reslist))
-names(NDVIforNN) <- sapply(1:9, function(i) paste("NDVI", as.character(i), sep = ""))
